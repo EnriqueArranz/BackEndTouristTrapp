19c06e674097075c1e1ab07a76f4915155f1ac5d
German Medina <germanmedinacalle@gmail.com>
2024-10-22 13:27:33 +0200

    models

--- src/main/java/com/example/Tourist_Trapp/model/culturalPlace.java
+++ src/main/java/com/example/Tourist_Trapp/model/culturalPlace.java
@@ -1,4 +1,35 @@
 package com.example.Tourist_Trapp.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class culturalPlace {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private double longitude;
+
+    @Column(nullable = false)
+    private double latitude;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private String address;
 }
--- src/main/java/com/example/Tourist_Trapp/model/turistConcentration.java
+++ src/main/java/com/example/Tourist_Trapp/model/turistConcentration.java
@@ -1,4 +1,31 @@
 package com.example.Tourist_Trapp.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class turistConcentration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private double longitude;
+
+    @Column(nullable = false)
+    private double latitude;
+
+    @Column(nullable = false)
+    private LocalDate date;
 }
