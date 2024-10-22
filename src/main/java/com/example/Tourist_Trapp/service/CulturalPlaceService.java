a7eff8185c5b9d0e09164337ee94991750befae5
Enrique Arranz <enrique.arrantzale@gmail.com>
2024-10-22 21:29:07 +0200

    t

--- src/main/java/com/example/Tourist_Trapp/service/CulturalPlaceService.java
+++ src/main/java/com/example/Tourist_Trapp/service/CulturalPlaceService.java
@@ -1,6 +1,8 @@
 package com.example.Tourist_Trapp.service;
 
 import com.example.Tourist_Trapp.model.CulturalPlace;
+import com.example.Tourist_Trapp.model.TouristConcentration;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +12,7 @@ public interface CulturalPlaceService {
     Optional<CulturalPlace> getCulturalPlaceById(Long id);
     CulturalPlace createCulturalPlace(CulturalPlace place);
     CulturalPlace updateCulturalPlace(Long id, CulturalPlace place);
+    ResponseEntity<List<CulturalPlace>> getAllPlaces();
     void deleteCulturalPlaceById(Long id);
     double getProximityPercentage(double poiLat, double poiLon, double maxDistance);
 }
