046659e0835bf94b1f9d099d98bca196b75f0ff6
Al-issawi <md.alisawi@gmail.com>
2024-10-22 20:08:35 +0200

    Creating Classes Noise Model  Contoroller service and repository

--- src/main/java/com/example/Tourist_Trapp/repository/NoiseRepository.java
+++ src/main/java/com/example/Tourist_Trapp/repository/NoiseRepository.java
@@ -1,8 +1,6 @@
 package com.example.Tourist_Trapp.repository;
 
-import com.example.Tourist_Trapp.model.CulturalPlace;
 import com.example.Tourist_Trapp.model.Noise;
-import com.example.Tourist_Trapp.model.TuristConcentration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
