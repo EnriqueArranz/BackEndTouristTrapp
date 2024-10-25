eef074f914ff38cd0b0dc220cbf88860a00e227e
enriqueArranz <enrique.arrantzale@gmail.com>
2024-10-25 16:56:04 +0200

    webconfig

---
+++ src/main/java/com/example/Tourist_Trapp/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.Tourist_Trapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Adjust this to your API endpoint pattern
+                .allowedOrigins("http://localhost:4200", "https://front-end-tourist-trapp.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Content-Type", "Authorization")
+                .maxAge(3600);
+    }
+}
