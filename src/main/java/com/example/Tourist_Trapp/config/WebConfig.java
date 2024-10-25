6587eb7f6bf465c1eb91afe7b841a402e1a31754
enriqueArranz <enrique.arrantzale@gmail.com>
2024-10-25 18:00:52 +0200

    options to cors

--- src/main/java/com/example/Tourist_Trapp/config/WebConfig.java
+++ src/main/java/com/example/Tourist_Trapp/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Adjust this to your API endpoint pattern
                 .allowedOrigins( "https://front-end-tourist-trapp.vercel.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization")
                 .maxAge(3600);
     }
